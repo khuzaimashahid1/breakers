e292159a5190612e493adbedde48907b9a3483f1
@Muhammad-Talha-Hashmi <abdulrab916@gmail.com>
2019-10-30 16:53:42 +0500

    hashni 007

--- electron/tables.js
+++ electron/tables.js
@@ -16,7 +16,6 @@ let parentWindow = remote.getCurrentWindow() //parentWindow
 
 const tab1 = document.getElementById('tab1');
 tab1.addEventListener('click',function(event){
-    console.log(remote.getGlobal('sharedObj').players)
     if(s1.innerHTML==="Vacant"){
         let winTab01= new BrowserWindow({
             parent:parentWindow,
@@ -25,15 +24,12 @@ tab1.addEventListener('click',function(event){
             webPreferences: {
                 nativeWindowOpen: true,
                 nodeIntegration: true,
-                webviewTag: true
-                
             }
         });
         winTab01.loadURL(url.format({
             pathname : path.join(__dirname,"./tab1Start.html"),
             protocol: "file",
-            slashes: "true",
-            
+            slashes: "true"
         }))
         remote.getGlobal('sharedObj').tableNumber = "Table1"
         // winTab01.on('close',function(event){
