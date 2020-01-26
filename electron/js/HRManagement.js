eed57799bbdb50ef50d745d76efa2e472a79b836
@abdulrab916 <talha.abdulrab@hypernymbiz.com>
2020-01-27 04:40:30 +0500

    HR Management UI issues Fixed!

--- electron/js/HRManagement.js
+++ electron/js/HRManagement.js
@@ -1,27 +1,19 @@
 require('datatables.net-dt')();
 
-function openModal(modalName)
-{
-    // Get the modal
-    var modal = document.getElementById(modalName);
-    modal.style.display = "block";
-    var close=modalName+'Close'
-    // Get the <span> element that closes the modal
-    var span = document.getElementById(close);
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () 
-    {
-        modal.style.display = "none";
+function openLink(evt, animName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) 
-    {
-      if (event.target == modal) 
-      {
-        modal.style.display = "none";
-      }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-white", "");
     }
+    document.getElementById(animName).style.display = "block";
+    evt.currentTarget.className += " w3-white";
 }
+
 
 function addEmployee()
 {
